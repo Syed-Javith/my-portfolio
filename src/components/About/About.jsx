253b8624a0642b8c7a6d19d1102b381b08871346
Syed-Javith <syedjavith14@gmail.com>
2024-01-18 20:13:09 +0530

    updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
 
         <div id="about" class="timeline">
         <div class="container left">
-          <div class="date">15 Dec</div>
+          {/* <div class="date">15 Dec</div> */}
           <i class="icon fa fa-home"></i>
           <div class="content">
             <h2>Lorem ipsum dolor sit amet</h2>
@@ -15,7 +15,7 @@ const About = () => {
           </div>
         </div>
         <div class="container right">
-          <div class="date">22 Oct</div>
+          {/* <div class="date">22 Oct</div> */}
           <i class="icon fa fa-gift"></i>
           <div class="content">
             <h2>Lorem ipsum dolor sit amet</h2>
